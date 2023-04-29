3116c3e6ae287f66024e9911e7c39521eec4d870
cleyton vinicius <cleytonterceiro@outlook.com>
2023-04-29 18:18:54 -0300

    lint

--- db/migrations/20230429210327_create_users_table.ts
+++ db/migrations/20230429210327_create_users_table.ts
@@ -1,5 +1,4 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
@@ -10,8 +9,6 @@ export async function up(knex: Knex): Promise<void> {
   })
 }
 
-
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable('users')
 }
-
